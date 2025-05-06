75778be91a7d6ce11bf67db13e800c662990496a
Bowie <rafael.ps.skt@gmail.com>
2025-05-06 00:46:41 -0300

    identificando caracteres

---
+++ identificando-caracteres.js
@@ -0,0 +1,36 @@
+const caractere = 'R';
+
+if (caractere === '') {
+    console.log('Digite um caractere válido');
+} else if (
+    caractere === 'A' || 
+    caractere === 'E' || 
+    caractere === 'I' || 
+    caractere === 'O' || 
+    caractere === 'U'
+) {
+    console.log('Vogal maiúscula');
+} else if (
+    caractere === 'a' || 
+    caractere === 'e' || 
+    caractere === 'i' || 
+    caractere === 'o' || 
+    caractere === 'u'
+) {
+    console.log('Vogal minúscula');
+} else if (
+    caractere === '1' || 
+    caractere === '2' || 
+    caractere === '3' || 
+    caractere === '4' || 
+    caractere === '5' || 
+    caractere === '6' || 
+    caractere === '7' || 
+    caractere === '8' || 
+    caractere === '9' || 
+    caractere === '0'
+) {
+    console.log('Número');
+} else {
+    console.log('Consoante');
+}
