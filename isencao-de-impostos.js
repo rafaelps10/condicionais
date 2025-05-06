5846bd4838192f3832b4fc95f4abc8165d7e21fe
Bowie <rafael.ps.skt@gmail.com>
2025-05-05 22:00:11 -0300

    lógica de isenção de imposto de renda

---
+++ isencao-de-impostos.js
@@ -0,0 +1,11 @@
+const aposentada = true;
+const portadoraDeDoenca = false;
+const totalDeRendimento = 3000000;
+
+if (aposentada || portadoraDeDoenca) {
+    console.log('ISENTA');
+} else if (totalDeRendimento < 2855970) {
+    console.log('VAZA, LEÃO! JÁ TÁ DIFÍCIL SEM VOCÊ')
+} else {
+    console.log('PEGA, LEÃO!')
+}
