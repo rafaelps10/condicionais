72ad02a1a49312afa0613234a2a35089840fd22d
Bowie <rafael.ps.skt@gmail.com>
2025-05-06 00:19:28 -0300

    código refatorado

--- transformando-nota-em-conceito.js
+++ transformando-nota-em-conceito.js
@@ -1,19 +1,3 @@
-// 1ª forma de resolver o problema:
-const nota = 4.5;
-
-if (nota >= 9 && nota <= 10) {
-    console.log('O estudando obteve conceito A');
-} else if (nota >= 8 && nota < 9 ) {
-    console.log('O estudando obteve conceito B');
-} else if (nota >= 6 && nota < 8 ) {
-    console.log('O estudando obteve conceito C');
-} else if (nota >= 4 && nota < 6 ) {
-    console.log('O estudando obteve conceito D');
-} else {
-    console.log('O estudando obteve conceito E');
-}
-
-// 2ª forma de resolver o problema:
 const nota2 = 4.5;
 
 let conceito = '';
