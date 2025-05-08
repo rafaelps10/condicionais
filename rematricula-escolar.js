3704e2d3d14b511bfb81b1632f9b4494a6ac245b
Bowie <rafael.ps.skt@gmail.com>
2025-05-07 21:31:43 -0300

    verificando se um aluno pode ou não se rematricular

---
+++ rematricula-escolar.js
@@ -0,0 +1,8 @@
+const idadeDoAluno = 16;
+const possuiResponsavel = false;
+
+if (idadeDoAluno < 18 && !possuiResponsavel) {
+    console.log('Não é possível fazer a rematrícula');
+} else {
+    console.log('Rematrícula realizada com sucesso!')
+}
